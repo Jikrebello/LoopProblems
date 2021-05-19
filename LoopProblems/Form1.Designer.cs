c3d2e087e9adf5439208c44fa47731ebaaf9a0ed
Jikrebello <33652252+Jikrebello@users.noreply.github.com>
2021-05-19 23:32:15 +0200

    Add files via upload

---
+++ LoopProblems/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace LoopProblems
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnPrintSteamBoat = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnLitresToGallons = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnMilesToKilometres = new System.Windows.Forms.Button();
+            this.BtnAreaCalculator = new System.Windows.Forms.Button();
+            this.BtnGradeCalculator = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnSumTo250 = new System.Windows.Forms.Button();
+            this.BtnOddNumbers29To99 = new System.Windows.Forms.Button();
+            this.BtnSumOfSquares = new System.Windows.Forms.Button();
+            this.BtnWeightsPerZone = new System.Windows.Forms.Button();
+            this.TxtAnswers = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnPrintSteamBoat
+            // 
+            this.BtnPrintSteamBoat.Location = new System.Drawing.Point(12, 34);
+            this.BtnPrintSteamBoat.Name = "BtnPrintSteamBoat";
+            this.BtnPrintSteamBoat.Size = new System.Drawing.Size(191, 40);
+            this.BtnPrintSteamBoat.TabIndex = 0;
+            this.BtnPrintSteamBoat.Text = "Print Steam Boat";
+            this.BtnPrintSteamBoat.UseVisualStyleBackColor = true;
+            this.BtnPrintSteamBoat.Click += new System.EventHandler(this.BtnPrintSteamBoat_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Counter Controlled Problems";
+            // 
+            // BtnLitresToGallons
+            // 
+            this.BtnLitresToGallons.Location = new System.Drawing.Point(12, 80);
+            this.BtnLitresToGallons.Name = "BtnLitresToGallons";
+            this.BtnLitresToGallons.Size = new System.Drawing.Size(191, 40);
+            this.BtnLitresToGallons.TabIndex = 2;
+            this.BtnLitresToGallons.Text = "Litres to Gallons";
+            this.BtnLitresToGallons.UseVisualStyleBackColor = true;
+            this.BtnLitresToGallons.Click += new System.EventHandler(this.BtnLitresToGallons_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "User Controlled Problems";
+            // 
+            // BtnMilesToKilometres
+            // 
+            this.BtnMilesToKilometres.Location = new System.Drawing.Point(12, 154);
+            this.BtnMilesToKilometres.Name = "BtnMilesToKilometres";
+            this.BtnMilesToKilometres.Size = new System.Drawing.Size(191, 40);
+            this.BtnMilesToKilometres.TabIndex = 4;
+            this.BtnMilesToKilometres.Text = "Miles to Kilometres";
+            this.BtnMilesToKilometres.UseVisualStyleBackColor = true;
+            this.BtnMilesToKilometres.Click += new System.EventHandler(this.BtnMilesToKilometres_Click);
+            // 
+            // BtnAreaCalculator
+            // 
+            this.BtnAreaCalculator.Location = new System.Drawing.Point(12, 200);
+            this.BtnAreaCalculator.Name = "BtnAreaCalculator";
+            this.BtnAreaCalculator.Size = new System.Drawing.Size(191, 40);
+            this.BtnAreaCalculator.TabIndex = 5;
+            this.BtnAreaCalculator.Text = "Area Calculator";
+            this.BtnAreaCalculator.UseVisualStyleBackColor = true;
+            this.BtnAreaCalculator.Click += new System.EventHandler(this.BtnAreaCalculator_Click);
+            // 
+            // BtnGradeCalculator
+            // 
+            this.BtnGradeCalculator.Location = new System.Drawing.Point(12, 246);
+            this.BtnGradeCalculator.Name = "BtnGradeCalculator";
+            this.BtnGradeCalculator.Size = new System.Drawing.Size(191, 40);
+            this.BtnGradeCalculator.TabIndex = 6;
+            this.BtnGradeCalculator.Text = "Grade Calculator";
+            this.BtnGradeCalculator.UseVisualStyleBackColor = true;
+            this.BtnGradeCalculator.Click += new System.EventHandler(this.BtnGradeCalculator_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "For/ Next Problems";
+            // 
+            // BtnSumTo250
+            // 
+            this.BtnSumTo250.Location = new System.Drawing.Point(12, 319);
+            this.BtnSumTo250.Name = "BtnSumTo250";
+            this.BtnSumTo250.Size = new System.Drawing.Size(191, 40);
+            this.BtnSumTo250.TabIndex = 8;
+            this.BtnSumTo250.Text = "Sum to 250";
+            this.BtnSumTo250.UseVisualStyleBackColor = true;
+            this.BtnSumTo250.Click += new System.EventHandler(this.BtnSumTo250_Click);
+            // 
+            // BtnOddNumbers29To99
+            // 
+            this.BtnOddNumbers29To99.Location = new System.Drawing.Point(12, 365);
+            this.BtnOddNumbers29To99.Name = "BtnOddNumbers29To99";
+            this.BtnOddNumbers29To99.Size = new System.Drawing.Size(191, 40);
+            this.BtnOddNumbers29To99.TabIndex = 9;
+            this.BtnOddNumbers29To99.Text = "Odd numbers from 29 to 99";
+            this.BtnOddNumbers29To99.UseVisualStyleBackColor = true;
+            this.BtnOddNumbers29To99.Click += new System.EventHandler(this.BtnOddNumbers29To99_Click);
+            // 
+            // BtnSumOfSquares
+            // 
+            this.BtnSumOfSquares.Location = new System.Drawing.Point(12, 411);
+            this.BtnSumOfSquares.Name = "BtnSumOfSquares";
+            this.BtnSumOfSquares.Size = new System.Drawing.Size(191, 40);
+            this.BtnSumOfSquares.TabIndex = 10;
+            this.BtnSumOfSquares.Text = "Sum of Squares";
+            this.BtnSumOfSquares.UseVisualStyleBackColor = true;
+            this.BtnSumOfSquares.Click += new System.EventHandler(this.BtnSumOfSquares_Click);
+            // 
+            // BtnWeightsPerZone
+            // 
+            this.BtnWeightsPerZone.Location = new System.Drawing.Point(12, 457);
+            this.BtnWeightsPerZone.Name = "BtnWeightsPerZone";
+            this.BtnWeightsPerZone.Size = new System.Drawing.Size(191, 40);
+            this.BtnWeightsPerZone.TabIndex = 11;
+            this.BtnWeightsPerZone.Text = "Weights Per Zone";
+            this.BtnWeightsPerZone.UseVisualStyleBackColor = true;
+            this.BtnWeightsPerZone.Click += new System.EventHandler(this.BtnWeightsPerZone_Click);
+            // 
+            // TxtAnswers
+            // 
+            this.TxtAnswers.Location = new System.Drawing.Point(236, 34);
+            this.TxtAnswers.Multiline = true;
+            this.TxtAnswers.Name = "TxtAnswers";
+            this.TxtAnswers.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtAnswers.Size = new System.Drawing.Size(229, 462);
+            this.TxtAnswers.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 508);
+            this.Controls.Add(this.TxtAnswers);
+            this.Controls.Add(this.BtnWeightsPerZone);
+            this.Controls.Add(this.BtnSumOfSquares);
+            this.Controls.Add(this.BtnOddNumbers29To99);
+            this.Controls.Add(this.BtnSumTo250);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BtnGradeCalculator);
+            this.Controls.Add(this.BtnAreaCalculator);
+            this.Controls.Add(this.BtnMilesToKilometres);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnLitresToGallons);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnPrintSteamBoat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnPrintSteamBoat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnLitresToGallons;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnMilesToKilometres;
+        private System.Windows.Forms.Button BtnAreaCalculator;
+        private System.Windows.Forms.Button BtnGradeCalculator;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BtnSumTo250;
+        private System.Windows.Forms.Button BtnOddNumbers29To99;
+        private System.Windows.Forms.Button BtnSumOfSquares;
+        private System.Windows.Forms.Button BtnWeightsPerZone;
+        private System.Windows.Forms.TextBox TxtAnswers;
+    }
+}
+
---
+++ LoopProblems/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Globalization;
+
+namespace LoopProblems
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* COUNTER CONTROLLED REPETITION PROBLEMS:
+         * 1. Create a program which will produce the following output:
+         * 1 steamboat
+         * 2 steamboat
+         * 3 steamboat
+         * CHARGE !
+        */
+        private void BtnPrintSteamBoat_Click(object sender, EventArgs e)
+        {
+            TxtAnswers.Text = "";
+
+            for (int i = 1; i < 4; i++)
+            {
+                TxtAnswers.Text += i + " steamboat" + Environment.NewLine;
+            }
+
+            TxtAnswers.Text += "CHARGE!";
+        }
+
+        /*
+         * 2. Create a program which will produce a table showing litre to gallons conversions for 20
+         * to 60 litres in increments of 4 litres. (1 gal = 4.545985 litres)
+        */
+        private void BtnLitresToGallons_Click(object sender, EventArgs e)
+        {
+            TxtAnswers.Text = "";
+
+            int gallon;
+            double litre = 4.545985f;
+            double conversion = 1;
+
+            TxtAnswers.Text += "Gallons" + "\t" + "Litres" + Environment.NewLine;
+
+            for (int i = 20; i <= 60; i += 4)
+            {
+                gallon = i;
+                conversion = litre * gallon;
+                TxtAnswers.Text += gallon.ToString() + "\t" + Math.Round(conversion, 2).ToString() + Environment.NewLine; ;
+            }
+        }
+
+        /* USER CONTROLLED REPETITION PROBLEMS:
+         * 1. Create a program which repeatedly prompts the user to enter miles and then outputs the
+         * equivalent measurement in kilometres. Make sure the miles entered are positive. Recall
+         * that 1 mile is equivalent to 1.61 kilometres.
+        */
+        private void BtnMilesToKilometres_Click(object sender, EventArgs e)
+        {
+            float miles = 0;
+            float kilometres;
+            const float conversion = 1.61f;
+            
+            while (true)
+            {
+                try
+                {
+                    miles = float.Parse(Interaction.InputBox("Enter the amount of Miles", "Miles to Kilometres", "", 200, 200), CultureInfo.InvariantCulture);
+
+                    if (miles < 0)
+                    {
+                        miles *= -1;
+                        MessageBox.Show("Converted Negative Value to Positive");
+                    }
+
+                    kilometres = miles * conversion;
+
+                    MessageBox.Show(kilometres + "kms  in " + miles + " miles");
+
+                    string response = Interaction.InputBox("Any more entries y/n", "Converter", "", 200, 200);
+                    if (response == "n")
+                        break;
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+        }
+
+        /*
+         * 2. Write a program which repeatedly prompts the user to enter a length and width and then
+         * outputs the area. Challenge: Try to check for invalid length and width entries.
+        */
+        private void BtnAreaCalculator_Click(object sender, EventArgs e)
+        {
+            float length = 0;
+            float width = 0;
+            float area;
+            
+            while (true)
+            {
+                try
+                {
+                    if (length < 0)
+                    {
+                        length *= -1;
+                        MessageBox.Show("Converted Negative Value to Positive");
+                    }
+
+                    if (width < 0)
+                    {
+                        width *= -1;
+                        MessageBox.Show("Converted Negative Value to Positive");
+                    }
+
+                    length = float.Parse(Interaction.InputBox("Enter the Length", "Area Calculator", "", 200, 200), CultureInfo.InvariantCulture);
+                    width = float.Parse(Interaction.InputBox("Enter the Width", "Area Calculator", "", 200, 200), CultureInfo.InvariantCulture);
+
+                    area = length * width;
+
+                    MessageBox.Show("The Area of " + length + " and " + width + ": " + Math.Round(area, 2));
+
+                    string response = Interaction.InputBox("Any more entries y/n", "Converter", "", 200, 200);
+                    if (response == "n")
+                        break;
+
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+        }
+
+        /*
+         * 3. Write a program which repeatedly accepts a mark and then determines the letter grade to
+         * assign it. Letter grades are assigned as follows.
+         * 0 <= mark < 50 F
+         * 50 <= mark < 60 D
+         * 60 <= mark < 70 C
+         * 70 <= mark < 80 B
+         * 80 <= mark < 100 A 
+        */
+        private void BtnGradeCalculator_Click(object sender, EventArgs e)
+        {
+            float mark;
+
+            while (true)
+            {
+                try
+                {
+                    mark = float.Parse(Interaction.InputBox("Enter in mark", "Grade Calculator", "", 200, 200), CultureInfo.InvariantCulture);
+
+                    if (mark < 50)
+                        MessageBox.Show("Grade is: F");
+                    else if (mark >= 50 && mark < 60)
+                        MessageBox.Show("Grade is: D");
+                    else if (mark >= 60 && mark < 70)
+                        MessageBox.Show("Grade is: C");
+                    else if (mark >= 70 && mark < 80)
+                        MessageBox.Show("Grade is: B");
+                    else
+                        MessageBox.Show("Grade is: A");
+
+                    string response = Interaction.InputBox("Any more entries y/n", "Converter", "", 200, 200);
+                    if (response == "n")
+                        break;
+
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+        }
+
+        /* FOR/NEXT PROBLEMS:
+         * 1. Write a program which sums the numbers 1,2,3,...250
+        */
+        private void BtnSumTo250_Click(object sender, EventArgs e)
+        {
+            TxtAnswers.Text = "";
+
+            int sum;
+
+            TxtAnswers.Text += "Number:" + "\t" + "Sum:" + Environment.NewLine;
+
+            for (int i = 1; i <= 250; i++)
+            {
+                sum = i + (i - 1);
+
+                TxtAnswers.Text += i.ToString() + "\t" + "(" + i + " + "+ (i-1) + ") = " + sum.ToString() + Environment.NewLine;
+            }
+        }
+
+        /*
+         * 2. Write a program which sums all the odd numbers from 27 to 99
+        */
+        private void BtnOddNumbers29To99_Click(object sender, EventArgs e)
+        {
+            TxtAnswers.Text = "";
+            
+            int sum;
+
+            TxtAnswers.Text += "Number:" + "\t" + "Sum:" + Environment.NewLine;
+
+            for (int i = 27; i <= 99; i += 2)
+            {
+                sum = i + (i - 2);
+
+                TxtAnswers.Text += i.ToString() + "\t(" + i + " + " + (i - 2) + ") = " + sum.ToString() + Environment.NewLine;
+            }
+        }
+
+        /*
+         * 3. Write a program which find the sum of the squares of the numbers 4,9,14,19,...39
+        */
+        private void BtnSumOfSquares_Click(object sender, EventArgs e)
+        {
+            TxtAnswers.Text = "";
+
+            double sum;
+
+            TxtAnswers.Text += "Number:" + "\t" + "Sum of Square:" + Environment.NewLine;
+
+            for (int i = 4; i <= 39; i++)
+            {
+                sum = Math.Sqrt(i) + Math.Sqrt(i - 2);
+
+                TxtAnswers.Text += i.ToString() + "\t(" + Math.Round(Math.Sqrt(i), 2) + " + " + Math.Round(Math.Sqrt(i - 2), 2) + ") = " + Math.Round(sum, 2).ToString() + Environment.NewLine;
+            }
+        }
+
+        /*
+         * 4. A delivery service charges its customers based on the weight of the package and the
+         * distance it must be shipped. It has divided the area it serves into zones. The company
+         * calculates the charge using the equation
+         * C = .75 + .03 x W x Z
+         * Write a program that will print a table of charges for weights of 2,4,6,8 and 10 and zones
+         * of 1, 2, and 3.
+        */
+        private void BtnWeightsPerZone_Click(object sender, EventArgs e)
+        {
+            TxtAnswers.Text = "";
+
+            float weight = 0;
+            float zone = 0;
+            float charge;
+
+            TxtAnswers.Text += "Weights:\tZone:\tCharge:" + Environment.NewLine;
+
+            for (int i = 2; i <= 10; i+= 2)
+            {
+                for (int j = 1; j <= 3 ; j++)
+                {
+                    weight = i;
+                    zone = j;
+                    charge = 0.75f + 0.3f * weight * zone;
+
+                    TxtAnswers.Text += i.ToString() + "\t" + j.ToString() + "\t" + charge + Environment.NewLine;
+                }
+            }
+        }
+
+    }
+}
